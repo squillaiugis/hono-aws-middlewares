6e420f0917b319760f09494016bb0ad97544001c
squillaiugis <r-shrimp@outlook.jp>
2025-08-04 23:23:41 +0900

    docs: add type definition for Hono environment variables including AWS services

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,10 @@ import { Env as S3Env, s3Middleware } from './s3';
 import { Env as SecretsManagerEnv, secretsManagerMiddleware } from './secrets-manager';
 
 export { dynamoDBMiddleware, s3Middleware, secretsManagerMiddleware };
+
+/**
+ * Type definition for Hono environment variables containing AWS services instances
+ *
+ * @public
+ */
 export type Env = DynamoDBEnv & SecretsManagerEnv & S3Env;
